1bd97c0aa3891c2a098dc4edbea74779d7a3a3e5
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-08-13 23:04:16 +0530

    Update sg90.c

--- sg90.c
+++ sg90.c
@@ -1,6 +1,8 @@
 #include "sg90.h"
+#include "esp_task_wdt.h"
 
 static const char TAG[8] = "pwm_tag";
+
 
  void servo_init(void)
 {
@@ -21,85 +23,88 @@ static const char TAG[8] = "pwm_tag";
         .timer_sel      = LEDC_TIMER,
         .intr_type      = LEDC_INTR_DISABLE,
         .gpio_num       = LEDC_OUTPUT_IO,
-        .duty           = LEDC_DUTY,
+        .duty           = 0,
         .hpoint         = 0
     };
     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
  void servo_swerve(void)
 {
-	servo_init();
-	static int servo_duty = 409;
-	while(1)
-	{
-	while(servo_duty <= 3277)
-	{
-		
-		servo_duty++;
-		ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, servo_duty);
-		ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-
-		ESP_LOGI(TAG,"0-180 sweep");
-	}
-	while(servo_duty>=1638)
-	{
-		servo_duty--;
-		ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, servo_duty);
-		ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-		ESP_LOGI(TAG,"180-0 sweep");
-	}
-	}
-	
+    static int servo_duty = 614;
+   
+    {
+        while(servo_duty <= 819)
+        {
+            servo_duty++;
+             ESP_LOGI(TAG,"Duty is %d",servo_duty);
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, servo_duty);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+            ESP_LOGI(TAG, "0-180 sweep");
+            vTaskDelay(50/ portTICK_PERIOD_MS);  // Small delay
+        }
+        
+        while(servo_duty >= 410)
+        {
+            
+            servo_duty--;
+              ESP_LOGI(TAG,"Duty is %d",servo_duty);
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, servo_duty);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+            ESP_LOGI(TAG, "180-0 sweep");
+            vTaskDelay(50/ portTICK_PERIOD_MS);  // Small delay
+        }
+    }
 }
 
-void servo_pos(int pos)
+
+ void servo_pos(int pos)
 {
-	servo_init();
+	
 	if(pos==1)
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 1638);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 407);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);	
 	}
 	else if (pos==2) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 1802);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 451);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}
 	else if (pos==3) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 1966);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 492);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==4) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 2130);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 532);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==5) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 2294);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 573);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==6) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 2458);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 614);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==7) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 2621);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 655);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==8) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 2785);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 696);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==9) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 2949);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 737);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==10) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 3113);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 778);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}else if (pos==11) 
 	{
-	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 3277);
+	ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 819);
 	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);		
 	}
 	else
